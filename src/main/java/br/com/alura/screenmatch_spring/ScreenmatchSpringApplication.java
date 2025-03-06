f61f8f94744b6da3b2d9b01759a87cb5b302e0fb
Miguel Kapicius <miguelkapicius@gmail.com>
2025-03-06 16:18:01 -0300

    second commit

---
+++ src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch_spring;
+
+import br.com.alura.screenmatch_spring.model.DadosSerie;
+import br.com.alura.screenmatch_spring.service.ConsumoAPI;
+import br.com.alura.screenmatch_spring.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3d33f15");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
